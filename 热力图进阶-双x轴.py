bf40cfe4168ebabd4c4889a1aea0d37d68115fc3
ixinyue <42425064+ixinyue94@users.noreply.github.com>
2019-10-28 19:18:08 +0800

    Update 热力图进阶-双x轴.py

--- 热力图进阶-双x轴.py
+++ 热力图进阶-双x轴.py
@@ -16,11 +16,11 @@ for i in range(len(df)):
 
 f, ax1 = plt.subplots(figsize=(9,6))
 sns.heatmap(flights,ax=ax1, cmap='YlOrRd',linewidths=0.5).invert_yaxis()
-ax1.set(xlabel = 'B', ylabel= 'A') #加轴标题
+ax1.set(xlabel = 'B', ylabel= 'A') 
 
-ax2 = ax1.twiny()
-sns.heatmap(flights,ax=ax2, cmap='YlOrRd',linewidths=0.5,xticklabels=c, cbar=False).invert_yaxis()
-ax2.xaxis.set_ticks_position('top')
-ax2.set(xlabel = 'C')
+ax2 = ax1.twiny() #新建ax1的镜像画布ax2
+sns.heatmap(flights,ax=ax2, cmap='YlOrRd',linewidths=0.5,xticklabels=c, cbar=False).invert_yaxis() #设置ax2的横坐标标签为c，不显示色轴
+ax2.xaxis.set_ticks_position('top') #设置ax2的横坐标位置为顶部
+ax2.set(xlabel = 'C') #设置ax2的横轴名字
 
 plt.show()
